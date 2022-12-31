ecbd4fdb80112a980249b8d5828445ef260d7efe
OhWhale515 <sterlinggreen515@gmail.com>
2022-12-31 16:28:32 -0500

    abs for diff percent and round for diff percent

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ TWILIO_AUTH_TOKEN = "e78c657bbfbc7d52c66f27a74098bc13"
     ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
-#TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
+# Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
 forex_params = {
     "function": "FX_DAILY",
     "from_symbol": FROM_FOREX_PAIR,
@@ -32,32 +32,32 @@ yesterday_closing_price = yesterday_data["4. close"]
 
 print(yesterday_closing_price)
 
-#TODO 2. - Get the day before yesterday's closing stock price
+#Get the day before yesterday's closing stock price
 day_before_yesterday_data = data_list[1]
 day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
 print(day_before_yesterday_closing_price)
 
 
-#TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
+#Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
 difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
-print(difference)
+up_down = None
+if difference > 0:
+    up_down = "⬆️"
+else:
+    up_down = "⬇️"
 
 
-#TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
-diff_percent = (difference / float(yesterday_closing_price)) * 100
+#Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
+diff_percent = round((difference / float(yesterday_closing_price)) * 100)
 print(diff_percent)
 
-
-#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
-if diff_percent > 1:
-    print("Get News")
 
 
     ## STEP 2: https://newsapi.org/ 
     # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
 
-#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
-if diff_percent > 1:
+#Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+if abs(diff_percent) > 1:
     news_params = {
         "apiKey": NEWS_API_KEY,
         "qInTitle": FROM_FOREX_PAIR, 
@@ -67,19 +67,26 @@ if diff_percent > 1:
     articles = news_response.json()["articles"]
     print(articles)
     
-#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+#Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
 three_articles = articles[:3]
 print(three_articles)
 
     ## STEP 3: Use twilio.com/docs/sms/quickstart/python
     #to send a separate message with each article's title and description to your phone number. 
 
-#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
+#Create a new list of the first 3 article's headline and description using list comprehension.
 formatted_articles = [f"Headline: {article['title']}. \nBrief: {article ['description']}" for article in three_articles]
 
-#TODO 9. - Send each article as a separate message via Twilio. 
 client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
 
+#Send each article as a separate message via Twilio. 
+for article in formatted_articles:
+    message = client.messages.create(
+        body=article,
+        from_="+16692682935",
+        to="+17252195413"
+    )
+    
 
 # #Optional TODO: Format the message like this: 
 # """
