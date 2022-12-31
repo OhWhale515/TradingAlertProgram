86f593e59a4c86feb247581e3d5ceb8cdf9d56e1
OhWhale515 <sterlinggreen515@gmail.com>
2022-12-31 14:59:45 -0500

    added if statement for diff percent

--- main.py
+++ main.py
@@ -39,12 +39,15 @@ print(difference)
 
 
 #TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
-diff_percent = (difference / yesterday_closing_price) * 100
+diff_percent = (difference / float(yesterday_closing_price)) * 100
 print(diff_percent)
 
 
-
 #TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
+if diff_percent > 5:
+    print("Get News")
+
+
 
     ## STEP 2: https://newsapi.org/ 
     # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
